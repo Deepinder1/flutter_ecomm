d018ab06577f984ad57f1924c4d9695717d5a673
Deepinder Singh <deep6421@gmail.com>
2020-10-22 19:40:28 +0530

    checked signin

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_ecom/business/screens/admin.dart';
 import 'package:flutter_ecom/user/helpers/navigations.dart';
 import 'package:flutter_ecom/user/models/product.dart';
 import 'package:flutter_ecom/user/screens/home.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,56 +18,520 @@ void main() async {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.amber),
-      home: ScreensController(),
+      home: MyApp(),
     ),
   );
 }
 
-class ScreensController extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]);
-    //TODO Add splash screen with logic
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Meet Up',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: IntroScreen(),
+    );
+  }
+}
+
+class IntroScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    User result = FirebaseAuth.instance.currentUser;
+    return new SplashScreen(
+        navigateAfterSeconds: result != null ? Home(uid: result.uid) : SignUp(),
+        seconds: 5,
+        title: new Text(
+          'Welcome To Meet up!',
+          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+        ),
+        // image: Image.asset('assets/images/dart.png', fit: BoxFit.scaleDown),
+        backgroundColor: Colors.white,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        onClick: () => print("flutter"),
+        loaderColor: Colors.red);
+  }
+}
+
+class Home extends StatelessWidget {
+  Home({this.uid});
+  final String uid;
+  final String title = "Home";
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            RaisedButton(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0),
-                  side: BorderSide(color: Colors.red)),
+        appBar: AppBar(
+          title: Text(title),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.exit_to_app,
+                color: Colors.white,
+              ),
               onPressed: () {
-                changeScreen(context, Admin());
+                FirebaseAuth auth = FirebaseAuth.instance;
+                auth.signOut().then((res) {
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignUp()),
+                      (Route<dynamic> route) => false);
+                });
               },
-              color: Colors.red,
-              textColor: Colors.white,
-              child: Text("Business Login".toUpperCase(),
-                  style: TextStyle(fontSize: 14)),
-            ),
-            SizedBox(height: 50),
-            RaisedButton(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0),
-                  side: BorderSide(color: Colors.red)),
-              onPressed: () {
-                changeScreen(context, HomePage());
-              },
-              color: Colors.red,
-              textColor: Colors.white,
-              child: Text("User Login".toUpperCase(),
-                  style: TextStyle(fontSize: 14)),
-            ),
+            )
           ],
+        ),
+        body: Center(child: Text('Welcome!')),
+        drawer: NavigateDrawer(uid: this.uid));
+  }
+}
+
+class NavigateDrawer extends StatefulWidget {
+  final String uid;
+  NavigateDrawer({Key key, this.uid}) : super(key: key);
+  @override
+  _NavigateDrawerState createState() => _NavigateDrawerState();
+}
+
+class _NavigateDrawerState extends State<NavigateDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountEmail: FutureBuilder(
+                future: FirebaseDatabase.instance
+                    .reference()
+                    .child("Users")
+                    .child(widget.uid)
+                    .once(),
+                builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
+                  if (snapshot.hasData) {
+                    return Text(snapshot.data.value['email']);
+                  } else {
+                    return CircularProgressIndicator();
+                  }
+                }),
+            accountName: FutureBuilder(
+                future: FirebaseDatabase.instance
+                    .reference()
+                    .child("Users")
+                    .child(widget.uid)
+                    .once(),
+                builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
+                  if (snapshot.hasData) {
+                    return Text(snapshot.data.value['name']);
+                  } else {
+                    return CircularProgressIndicator();
+                  }
+                }),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+          ),
+          ListTile(
+            leading: new IconButton(
+              icon: new Icon(Icons.home, color: Colors.black),
+              onPressed: () => null,
+            ),
+            title: Text('Home'),
+            onTap: () {
+              print(widget.uid);
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Home(uid: widget.uid)),
+              );
+            },
+          ),
+          ListTile(
+            leading: new IconButton(
+              icon: new Icon(Icons.settings, color: Colors.black),
+              onPressed: () => null,
+            ),
+            title: Text('Settings'),
+            onTap: () {
+              print(widget.uid);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SignUp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('title'),
+        ),
+        body: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+              Widget>[
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text("Meet Up",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  )),
+            ),
+            Padding(
+                padding: EdgeInsets.all(10.0),
+                child: SignInButton(
+                  Buttons.Email,
+                  text: "Sign up with Email",
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => EmailSignUp()),
+                    );
+                  },
+                )),
+            Padding(
+                padding: EdgeInsets.all(10.0),
+                child: GestureDetector(
+                    child: Text("Log In Using Email",
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Colors.blue)),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => EmailLogIn()),
+                      );
+                    }))
+          ]),
+        ));
+  }
+}
+
+//creating email signup
+class EmailSignUp extends StatefulWidget {
+  @override
+  _EmailSignUpState createState() => _EmailSignUpState();
+}
+
+class _EmailSignUpState extends State<EmailSignUp> {
+  final _formKey = GlobalKey<FormState>();
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  DatabaseReference dbRef =
+      FirebaseDatabase.instance.reference().child("Users");
+  TextEditingController emailController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
+
+  //creating function registerToFb
+  void registerToFb() {
+    firebaseAuth
+        .createUserWithEmailAndPassword(
+            email: emailController.text, password: passwordController.text)
+        .then((result) {
+      dbRef.child(result.user.uid).set({
+        "email": emailController.text,
+        "age": ageController.text,
+        "name": nameController.text
+      }).then((res) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => Home(uid: result.user.uid)),
+        );
+      });
+    }).catchError((err) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text("Error"),
+              content: Text(err.message),
+              actions: [
+                FlatButton(
+                  child: Text("Ok"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            );
+          });
+    });
+  }
+
+  //disposing
+  @override
+  void dispose() {
+    super.dispose();
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    ageController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: TextFormField(
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    labelText: 'Enter Username',
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Enter Username';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    labelText: 'Enter Email',
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Enter Email';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: TextFormField(
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                    labelText: 'Enter Password',
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Enter Password';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: TextFormField(
+                  controller: ageController,
+                  decoration: InputDecoration(
+                    labelText: 'Enter Age',
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Enter Age';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: RaisedButton(
+                  color: Colors.lightBlue,
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      registerToFb();
+                    }
+                  },
+                  child: Text('Submit'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
-    // return HomePage();
   }
 }
+
+//creating email login
+class EmailLogIn extends StatefulWidget {
+  @override
+  _EmailLogInState createState() => _EmailLogInState();
+}
+
+class _EmailLogInState extends State<EmailLogIn> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  bool isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Login")),
+        body: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+                child: Column(children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    labelText: "Enter Email Address",
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Enter Email Address';
+                    } else if (!value.contains('@')) {
+                      return 'Please enter a valid email address!';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: TextFormField(
+                  obscureText: true,
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                    labelText: "Enter Password",
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Enter Password';
+                    } else if (value.length < 6) {
+                      return 'Password must be atleast 6 characters!';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: isLoading
+                    ? CircularProgressIndicator()
+                    : RaisedButton(
+                        color: Colors.lightBlue,
+                        onPressed: () {
+                          if (_formKey.currentState.validate()) {
+                            setState(() {
+                              isLoading = true;
+                            });
+                            logInToFb();
+                          }
+                        },
+                        child: Text('Submit'),
+                      ),
+              )
+            ]))));
+  }
+
+  void logInToFb() {
+    FirebaseAuth.instance
+        .signInWithEmailAndPassword(
+            email: emailController.text, password: passwordController.text)
+        .then((result) {
+      isLoading = false;
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Home(uid: result.user.uid)),
+      );
+    }).catchError((err) {
+      print(err.message);
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text("Error"),
+              content: Text(err.message),
+              actions: [
+                FlatButton(
+                  child: Text("Ok"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            );
+          });
+    });
+  }
+}
+
+// class ScreensController extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     SystemChrome.setPreferredOrientations([
+//       DeviceOrientation.portraitUp,
+//       DeviceOrientation.portraitDown,
+//     ]);
+//     //TODO Add splash screen with logic
+//     return Scaffold(
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             RaisedButton(
+//               shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.circular(18.0),
+//                   side: BorderSide(color: Colors.red)),
+//               onPressed: () {
+//                 changeScreen(context, Admin());
+//               },
+//               color: Colors.red,
+//               textColor: Colors.white,
+//               child: Text("Business Login".toUpperCase(),
+//                   style: TextStyle(fontSize: 14)),
+//             ),
+//             SizedBox(height: 50),
+//             RaisedButton(
+//               shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.circular(18.0),
+//                   side: BorderSide(color: Colors.red)),
+//               onPressed: () {
+//                 changeScreen(context, HomePage());
+//               },
+//               color: Colors.red,
+//               textColor: Colors.white,
+//               child: Text("User Login".toUpperCase(),
+//                   style: TextStyle(fontSize: 14)),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//     // return HomePage();
+//   }
+// }
 //TODO Refactor code as much possible
 
 // Start-up logo screen for the app
