ec661571cc34af999b60adf0f72ac8c8ee07931b
Deepinder Singh <deep6421@gmail.com>
2020-10-24 16:57:25 +0530

    logins

---
+++ lib/user/provider/user.dart
@@ -0,0 +1,147 @@
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_ecom/user/models/cart_item.dart';
+import 'package:flutter_ecom/user/models/order.dart';
+import 'package:flutter_ecom/user/models/product.dart';
+import 'package:flutter_ecom/user/models/user.dart';
+import 'package:flutter_ecom/user/services/order.dart';
+import 'package:flutter_ecom/user/services/users.dart';
+import 'package:uuid/uuid.dart';
+import 'package:flutter/widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+enum Status { Uninitialized, Authenticated, Authenticating, Unauthenticated }
+
+class UserProvider with ChangeNotifier {
+  FirebaseAuth _auth;
+  User _user;
+  Status _status = Status.Uninitialized;
+  UserServices _userServices = UserServices();
+  OrderServices _orderServices = OrderServices();
+
+  UserModel _userModel;
+
+  //getters
+  UserModel get userModel => _userModel;
+
+  Status get status => _status;
+
+  User get user => _user;
+
+  //public variables
+  List<OrderModel> orders = [];
+
+  //constructor
+  UserProvider.initialize() : _auth = FirebaseAuth.instance {
+    _auth.authStateChanges().listen((_onStateChanged));
+  }
+// in all these functions we are using enums in _status
+//because we ca use in Switch case for changing screens or for doing anything
+  Future<bool> signIn(String email, String password) async {
+    try {
+      _status = Status.Authenticating;
+      notifyListeners();
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return true;
+    } catch (e) {
+      _status = Status.Unauthenticated;
+      notifyListeners();
+      print('The error for signIn is: ${e.toString()}');
+      return false;
+    }
+  }
+
+  Future<bool> signUp(String name, String email, String password) async {
+    try {
+      _status = Status.Authenticating;
+      notifyListeners();
+      await _auth
+          .createUserWithEmailAndPassword(email: email, password: password)
+          .then((user) {
+        _userServices.createUser({
+          'name': name,
+          'email': email,
+          'uid': user.user.uid,
+          'stripeId': ''
+        });
+      });
+      return true;
+    } catch (e) {
+      _status = Status.Unauthenticated;
+      notifyListeners();
+      print('The error for signUp is: ${e.toString()}');
+      return false;
+    }
+  }
+
+  Future signOut() async {
+    _auth.signOut();
+    _status = Status.Unauthenticated;
+    notifyListeners();
+    return Future.delayed(Duration.zero);
+  }
+
+  Future<void> _onStateChanged(User user) async {
+    if (user == null) {
+      _status = Status.Unauthenticated;
+    } else {
+      _user = user;
+      _userModel = await _userServices.getUserById(user.uid);
+      _status = Status.Authenticated;
+    }
+    notifyListeners();
+  }
+
+  Future<bool> addToCart(
+      {ProductModel product, String size, String color}) async {
+    try {
+      var uuid = Uuid();
+      String cartItemId = uuid.v4();
+      List<CartItemModel> cart = _userModel.cart;
+
+      Map cartItem = {
+        'id': cartItemId,
+        'name': product.name,
+        'image': product.picture,
+        'productId': product.id,
+        'price': product.price,
+        'size': size,
+        'color': color
+      };
+
+      CartItemModel item = CartItemModel.fromMap(cartItem);
+
+      print('Cart items are : ${cart.toString()}');
+      _userServices.addToCart(userId: _user.uid, cartItem: item);
+
+      return true;
+    } catch (e) {
+      print('The error for adding from cart is: ${e.toString()}');
+      return false;
+    }
+  }
+
+  Future<bool> removeFromCart({CartItemModel cartItem}) async {
+    print('The product is : ${cartItem.toString()}');
+
+    try {
+      _userServices.removeFromCart(userId: _user.uid, cartItem: cartItem);
+      return true;
+    } catch (e) {
+      print('The error for removing from cart is: ${e.toString()}');
+      return false;
+    }
+  }
+
+  //it might return future or streams
+  getOrders() async {
+    orders = await _orderServices.getUserOrders(userId: _user.uid);
+    notifyListeners();
+  }
+
+  Future<void> reloadUserModel() async {
+    _userModel = await _userServices.getUserById(user.uid);
+    notifyListeners();
+  }
+}
---
+++ lib/user/screens/login.dart
@@ -0,0 +1,228 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecom/user/helpers/navigations.dart';
+import 'package:flutter_ecom/user/helpers/style.dart';
+import 'package:flutter_ecom/user/provider/user.dart';
+import 'package:flutter_ecom/user/screens/home.dart';
+import 'package:flutter_ecom/user/screens/signup.dart';
+import 'package:flutter_ecom/user/widgets/loading.dart';
+import 'package:provider/provider.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+//creating keys and controllers
+  final _formKey = GlobalKey<FormState>();
+  final _key = GlobalKey<ScaffoldState>();
+
+  TextEditingController _email = TextEditingController();
+  TextEditingController _password = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    //TODO see if listen : false have to be set in this provider
+    final user = Provider.of<UserProvider>(context);
+
+    return Scaffold(
+      key: _key,
+      // TODO instead of Loading put Splash() screen also
+      body: user.status == Status.Authenticating
+          ? Loading()
+          : Stack(
+              children: <Widget>[
+                Container(
+                  child: Padding(
+                    padding: const EdgeInsets.all(0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: white,
+                        borderRadius: BorderRadius.circular(16),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey[350],
+                            blurRadius:
+                                20.0, // has the effect of softening the shadow
+                          ),
+                        ],
+                      ),
+                      child: Form(
+                        key: _formKey,
+                        child: ListView(
+                          children: <Widget>[
+                            SizedBox(height: 40),
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              //here can put LOGO
+                              child: Container(
+                                alignment: Alignment.topCenter,
+                                child: Icon(
+                                  Icons.shopping_cart,
+                                  size: 50,
+                                  color: Colors.amber,
+                                ),
+                              ),
+                            ),
+
+                            //email
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(
+                                  14.0, 8.0, 14.0, 8.0),
+                              child: Material(
+                                borderRadius: BorderRadius.circular(10.0),
+                                color: Colors.grey.withOpacity(0.3),
+                                elevation: 0.0,
+                                child: Padding(
+                                  padding: const EdgeInsets.only(left: 12.0),
+                                  child: TextFormField(
+                                    controller: _email,
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: "Email",
+                                      icon: Icon(Icons.alternate_email),
+                                    ),
+                                    validator: (value) {
+                                      if (value.isEmpty) {
+                                        Pattern pattern =
+                                            r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                                        RegExp regex = new RegExp(pattern);
+                                        if (!regex.hasMatch(value))
+                                          return 'Please make sure your email address is valid';
+                                        else
+                                          return null;
+                                      }
+                                    },
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                            //password
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(
+                                  14.0, 8.0, 14.0, 8.0),
+                              child: Material(
+                                borderRadius: BorderRadius.circular(10.0),
+                                color: Colors.grey.withOpacity(0.3),
+                                elevation: 0.0,
+                                child: Padding(
+                                  padding: const EdgeInsets.only(left: 12.0),
+                                  child: TextFormField(
+                                    controller: _password,
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: "Password",
+                                      icon: Icon(Icons.lock_outline),
+                                    ),
+                                    validator: (value) {
+                                      if (value.isEmpty) {
+                                        return "The password field cannot be empty";
+                                      } else if (value.length < 6) {
+                                        return "the password has to be at least 6 characters long";
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                            //signin button
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(
+                                  14.0, 8.0, 14.0, 8.0),
+                              child: Material(
+                                borderRadius: BorderRadius.circular(20.0),
+                                color: Colors.black,
+                                elevation: 0.0,
+                                child: MaterialButton(
+                                  onPressed: () async {
+                                    if (_formKey.currentState.validate()) {
+                                      if (!await user.signIn(
+                                          _email.text, _password.text)) {
+                                        _key.currentState.showSnackBar(SnackBar(
+                                            content: Text('Sign In failed')));
+                                        return; //TODOsee if not required
+                                      }
+                                      changeScreenReplacement(
+                                          context, HomePage());
+                                    }
+                                  },
+                                  minWidth: MediaQuery.of(context).size.width,
+                                  child: Text(
+                                    "Login",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20.0),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            //forgot password
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    "Forgot password",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: black,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: InkWell(
+                                        onTap: () {
+                                          Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      SignUp()));
+                                        },
+                                        child: Text(
+                                          "Create an account",
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(color: Colors.black),
+                                        ))),
+                              ],
+                            ),
+                            //if to make google signin
+
+//                        Padding(
+//                          padding: const EdgeInsets.all(16.0),
+//                          child: Row(
+//                            mainAxisAlignment: MainAxisAlignment.center,
+//                            children: <Widget>[
+//
+//                              Padding(
+//                                padding: const EdgeInsets.all(8.0),
+//                                child: Text("or sign in with", style: TextStyle(fontSize: 18,color: Colors.grey),),
+//                              ),
+//                              Padding(
+//                                padding: const EdgeInsets.all(8.0),
+//                                child: MaterialButton(
+//                                    onPressed: () {},
+//                                    child: Image.asset("images/ggg.png", width: 30,)
+//                                ),
+//                              ),
+//
+//                            ],
+//                          ),
+//                        ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+    );
+  }
+}
